068820a78aec806c9f79a5bd1c48a0be9aea8d44
Niraj <mathersnepal@gmail.com>
2022-10-28 22:27:03 -0400

    initial commit

---
+++ app/screens/MainScreen.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, StyleSheet, Text } from 'react-native';
+
+const mainScreen = () => {
+    return (
+        <>
+        <StatusBar/>
+        <View>
+            <Text>Hello</Text>
+        </View>
+        </>
+    )
+}
+
+export default mainScreen;``
