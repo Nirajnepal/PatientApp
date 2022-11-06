e4432cd5e79a8544710daa7a99b9242c308724a8
Krisuv16 <krisuv16@gmail.com>
2022-11-06 13:56:05 -0500

    Add Patient Page

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from "../app/screens/Home";
 import PatientDetails from "../app/screens/PatientDetails";
 import PatientsLists from '../app/screens/patientLists';
+import AddPatientScreen from "../app/screens/addPatient";
 
 const Stack = createStackNavigator()
 
@@ -11,6 +12,7 @@ export default function Navigator() {
         <Stack.Screen name='Home' component={ Home } />
         <Stack.Screen name='PatientDetails' component={ PatientDetails } />
         <Stack.Screen name='PatientLists' component={ PatientsLists } />
+        <Stack.Screen name='Add Patients' component={ AddPatientScreen } />
       </Stack.Navigator>
   );
 }
